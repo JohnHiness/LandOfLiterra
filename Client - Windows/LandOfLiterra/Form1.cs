d5f3a6f680b3d2435553902053d7ba455c8199f2
johanhoiness <johan.sina.hoiness@gmail.com>
2015-10-10 00:05:04 +0200

    Added log-in window to initialize on beforehand.

--- Client - Windows/LandOfLiterra/Form1.cs
+++ Client - Windows/LandOfLiterra/Form1.cs
@@ -12,8 +12,17 @@ namespace LandOfLiterra
 {
     public partial class Form1 : Form
     {
+        bool loggedIn = false;
+        public void startup()
+        {
+            
+        }
         public Form1()
         {
+            // string promptValue = LogIn.ShowDialog("Test", "Test2");
+            Form2 loginForm = new Form2();
+            loginForm.ShowDialog();
+            if (loggedIn == false) { Application.Exit(); Environment.Exit(1); }
             InitializeComponent();
         }
 
@@ -29,4 +38,50 @@ namespace LandOfLiterra
             }
         }
     }
+    /**
+    public static class LogIn
+    {
+        public static string ShowDialog(string text, string caption)
+        {
+            Form LogIn = new Form();
+            LogIn.Width = 500;
+            LogIn.Height = 400;
+            LogIn.FormBorderStyle = FormBorderStyle.FixedDialog;
+            LogIn.Text = caption;
+            LogIn.StartPosition = FormStartPosition.CenterScreen;
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = text };
+            TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
+            Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+            confirmation.Click += (sender, e) => { LogIn.Close(); };
+            LogIn.Controls.Add(textBox);
+            LogIn.Controls.Add(confirmation);
+            LogIn.Controls.Add(textLabel);
+            LogIn.AcceptButton = confirmation;
+
+            return LogIn.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+        }
+    }
+    public static class SelServer
+    {
+        public static string ShowDialog(string text, string caption)
+        {
+            Form SelServer = new Form();
+            SelServer.Width = 500;
+            SelServer.Height = 150;
+            SelServer.FormBorderStyle = FormBorderStyle.FixedDialog;
+            SelServer.Text = caption;
+            SelServer.StartPosition = FormStartPosition.CenterScreen;
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = text };
+            TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
+            Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+            confirmation.Click += (sender, e) => { SelServer.Close(); };
+            SelServer.Controls.Add(textBox);
+            SelServer.Controls.Add(confirmation);
+            SelServer.Controls.Add(textLabel);
+            SelServer.AcceptButton = confirmation;
+
+            return SelServer.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+        }
+    }
+    **/
 }
---
+++ Client - Windows/LandOfLiterra/Form2.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace LandOfLiterra
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUsername = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.Location = new System.Drawing.Point(76, 113);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(242, 20);
+            this.lblUsername.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Nirmala UI Semilight", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(95, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(203, 41);
+            this.lblTitle.TabIndex = 4;
+            this.lblTitle.Text = "Land of Littera";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(154, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "- - Log in - -";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.Location = new System.Drawing.Point(76, 139);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(242, 20);
+            this.lblPassword.TabIndex = 6;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 227);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblUsername);
+            this.Name = "Form2";
+            this.Text = "Land of Literra - Log in";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox lblUsername;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label label1;
+        protected internal System.Windows.Forms.TextBox lblPassword;
+    }
+}
